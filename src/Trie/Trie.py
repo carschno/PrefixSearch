0984fb8f81ff6933d7ca53fa9a5a1bc88cc29098
Carsten Schnober <carschno@gmail.com>
2014-11-16 16:18:18 +0100

    Docstrings added.

--- src/Trie/Trie.py
+++ src/Trie/Trie.py
@@ -5,22 +5,28 @@ class Trie:
         self.name = name;
         
     def insert(self, path=[], value=1):
+        """Insert new branch / sub-tree with the given weight (value)."""
         if path:
             child = self.getChild(path[0])
             if child:
+                # child with given name (path) found, append
                 child.weight += value
             else:
+                # no child with given name (path) found, create new branch
                 child = Trie(path[0], value)
                 self.children.append(child)    
             child.insert(path[1:], value)
             
     def findMinimal(self, minLevel=0, prefix=""):
+        """Find the smallest common sub-tree and return the tuple (name, value)."""
         minimals = []
         if minLevel == 0:
             # minimal depth in tree reached
             if len(self.children) == 1:
+                # only one child, go down
                 map(minimals.append, self.children[0].findMinimal(minLevel, prefix + "/" + self.name))
             else:
+                # multiple branches found, attach current node as minimal
                 minimals.append((prefix + "/" + self.name, self.weight)) 
         else:
             # go deeper into the tree
@@ -30,15 +36,18 @@ class Trie:
             
     
     def __str__(self, indent=0):
+        """Return a string representation of the tree."""
         output = "  " * indent + "'" + self.name + "' " + str(self.weight) + "\n"
         for child in self.children:
             output += child.__str__(indent + 2)
         return output
 
     def getChild(self, name):
+        """Return the child with the given name or False if there is none."""
         result = False
         for child in self.children:
             if child.name == name:
+                # matching child found
                 result = child
                 break
         return result
